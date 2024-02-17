0d6d933692d9177c2bdab321594d46372511658a
Kanthasuwamy-Kumaran <rkkumaran834@gmail.com>
2024-02-17 07:22:11 +0530

    new31

--- frontend/src/components/Avatardemo.jsx
+++ frontend/src/components/Avatardemo.jsx
@@ -242,7 +242,7 @@ export default function Avatardemo() {
 
             <Iframe url="https://nolimite.readyplayer.me?frameApi"
                 width="100%"
-                height="900px"
+                height="910px"
                 allow="camera *; microphone *; clipboard-write"    
                  />
                  
